360476a923ae23790fccae29bc5fe8452d74d058
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-05-02 18:35:21 -0400

    Creatde NavBar, added styling and content

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'event-list' }">List</router-link> |
-      <router-link :to="{ name: 'event-create' }">Create</router-link>
-    </div>
-    <router-view />
-  </div>
+    
 </template>
 
 <style>
